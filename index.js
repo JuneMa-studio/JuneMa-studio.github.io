ed6bfa4c11fbe92d286aadab9ae777fb8e524ba4
马俊武 <76120075+JuneMa-studio@users.noreply.github.com>
2023-01-14 16:00:36 +0800

    01/14
    none

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 // index
+window.onload = function (){
+	var homep = document.querySelector('#home_page');
+	homep.className = 'roll_item_page_active';
+}
 function rollmenuact(e){
 	var home = document.querySelector("#home").style;
 	var note = document.querySelector("#note").style;
 	var project = document.querySelector("#project").style;
 	var other = document.querySelector("#other").style;
-	var estyle = e.style;
+	var homep = document.querySelector('#home_page');
+	var notep = document.querySelector('#note_page');
+	var projectp = document.querySelector('#project_page');
+	var otherp = document.querySelector('#other_page');
 	switch(e.id){
-		case 'home' : estyle.width = '40vw';estyle.fontWeight = '600';
-			note.width = '20vw';note.fontWeight = '300';
-			project.width = '20vw';project.fontWeight = '300';
-			other.width = '20vw';other.fontWeight = '300';break;
-		case 'note' : estyle.width = '40vw';estyle.fontWeight = '600';
-			home.width = '20vw';home.fontWeight = '300';
-			project.width = '20vw';project.fontWeight = '300';
-			other.width = '20vw';other.fontWeight = '300';break;
-		case 'project' : estyle.width = '40vw';estyle.fontWeight = '600';
-			home.width = '20vw';home.fontWeight = '300';
-			note.width = '20vw';note.fontWeight = '300';
-			other.width = '20vw';other.fontWeight = '300';break;
-		case 'other' : estyle.width = '40vw';estyle.fontWeight = '600';
-			home.width = '20vw';home.fontWeight = '300';
-			note.width = '20vw';note.fontWeight = '300';
-			project.width = '20vw';project.fontWeight = '300';break;
+		case 'home' : home.width = '40vw';home.fontWeight = '600';homep.className = 'roll_item_page_active';
+			note.width = '20vw';note.fontWeight = '300';notep.className = 'roll_item_page';
+			project.width = '20vw';project.fontWeight = '300';projectp.className = 'roll_item_page';
+			other.width = '20vw';other.fontWeight = '300';otherp.className = 'roll_item_page';
+			break;
+		case 'note' : note.width = '40vw';note.fontWeight = '600';notep.className = 'roll_item_page_active';
+			home.width = '20vw';home.fontWeight = '300';homep.className = 'roll_item_page';
+			project.width = '20vw';project.fontWeight = '300';projectp.className = 'roll_item_page';
+			other.width = '20vw';other.fontWeight = '300';otherp.className = 'roll_item_page';
+			break;
+		case 'project' : project.width = '40vw';project.fontWeight = '600';projectp.className = 'roll_item_page_active';
+			home.width = '20vw';home.fontWeight = '300';homep.className = 'roll_item_page';
+			note.width = '20vw';note.fontWeight = '300';notep.className = 'roll_item_page';
+			other.width = '20vw';other.fontWeight = '300';otherp.className = 'roll_item_page';
+			break;
+		case 'other' : other.width = '40vw';other.fontWeight = '600';otherp.className = 'roll_item_page_active';
+			home.width = '20vw';home.fontWeight = '300';homep.className = 'roll_item_page';
+			note.width = '20vw';note.fontWeight = '300';notep.className = 'roll_item_page';
+			project.width = '20vw';project.fontWeight = '300';projectp.className = 'roll_item_page';
+			break;
 		default : console.log('error');
 	}
 }
+
